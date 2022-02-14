0857507901028d7ce759ad6f99413e656c8cbd5c
maheshsass <ror.shaan@gmail.com>
2022-02-14 15:07:18 +0530

    first commit

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,14 @@
+class ShopsController < ApplicationController
+  def index
+  end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
---
+++ db/migrate/20220214092230_create_shops.rb
@@ -0,0 +1,12 @@
+class CreateShops < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shops do |t|
+      t.string :shop_name,       null: false
+      t.string :address,         null: false
+      t.integer :chair,          null: false           
+      t.datetime :opening_time,  null: false
+      t.datetime :closing_time,  null: false
+      t.timestamps
+    end
+  end
+end
